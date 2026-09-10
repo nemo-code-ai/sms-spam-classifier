aea08ad9e43f38169fc9392da4c7094b5e75c6f4
nemo-code-ai <themichaelugwumbanwoye@gmail.com>
2026-09-10 17:29:52 +0100

    +-

--- src/sms_spam_classifier/app.py
+++ src/sms_spam_classifier/app.py
@@ -9,6 +9,12 @@ vectorizer = joblib.load(BASE_DIR / "vectorizer.pkl")
 
 st.title("SMS Spam Classifier")
 
+st.write(
+    "This is a simple machine learning that can take any SMS and determine whether it is SPAM or a legitimate message(HAM)"
+)
+
+st.write("Enter any message you have received from your SMS")
+
 message = st.text_area("Enter SMS Message")
 
 if st.button("Text Message"):
@@ -19,6 +25,8 @@ if st.button("Text Message"):
     model_mess = model.predict_proba(vector_mess)[0][1]
 
     if model_mess >= 0.5:
-        st.success("This message is SPAM")
+        st.error("This message is SPAM")
     else:
-        st.info("This message is HAM")
+        st.success("This message is legitimate (HAM)")
+
+st.caption("Built using Balanced Logistic Regression and Bag of Words")
